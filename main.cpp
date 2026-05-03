617feb13a1d0dd958a90838eb716508f397b220f
Reak <feastcorere@gmail.com>
2026-05-03 16:38:18 +0200

    Aktualisieren von main.cpp

--- main.cpp
+++ main.cpp
@@ -5,16 +5,16 @@
 int main() {
     srand(time(0));
 
-    int geheimeZahl = rand() % 100 + 1;
+    int secretnumber = rand() % 100 + 1;
     int input = 0;
 
-    while (input != geheimeZahl) {
+    while (input != secretnumber) {
         std::cout << "Guess a number between 1 and 100: " << std::flush;
         std::cin >> input;
 
-        if (input < geheimeZahl) {
+        if (input < secretnumber) {
             std::cout << "To Low!\n";
-        } else if (input > geheimeZahl) {
+        } else if (input > secretnumber) {
             std::cout << "To High!\n";
         } else {
             std::cout << "You Win!\n";
